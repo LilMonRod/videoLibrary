1d92d123394eda8bde51b6bd3e7af37d7363eb5b
l.montoya.rodriguez <l.montoya.rodriguez@accenture.com>
2022-09-20 13:35:31 -0600

    adding contact tap

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let btnContacto = document.getElementById("btnContacto");
+let contContacto = document.getElementById("contacto");
+
+let btnLibrary = document.getElementById("btnLibrary");
+let contLibrary = document.getElementById("library");
+
+btnLibrary.addEventListener("click", function() {
+    btnLibrary.style.textDecoration = "underline";
+    btnContacto.style.textDecoration = "none";
+
+    contContacto.style.display = "none";
+    contLibrary.style.display = "grid";
+});
+
+btnContacto.addEventListener("click", function() {
+    btnContacto.style.textDecoration = "underline";
+    btnLibrary.style.textDecoration = "none";
+
+    contContacto.style.display = "flex";
+    contLibrary.style.display = "none";
+
+    console.log("contacto muestra:")
+    console.log(btnContacto)
+    console.log(contContacto)
+});
